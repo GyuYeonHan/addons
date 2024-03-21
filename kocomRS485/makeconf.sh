8f90a8c6f08ce32a407e60396e559d182751bb68
GyeYeonHan <gks951020@gmail.com>
2024-03-21 21:53:53 +0900

    1

--- kocomRS485/makeconf.sh
+++ kocomRS485/makeconf.sh
@@ -3,17 +3,30 @@
 CONFIG_FILE=/data/options.json
 CONFIG_RS485=/share/kocom/rs485.conf
 
-CONFIG=`cat $CONFIG_FILE`
+# Read contents of CONFIG_FILE into a variable
+CONFIG=$(cat "$CONFIG_FILE")
 
-> $CONFIG_RS485
+# Clear the contents of CONFIG_RS485
+> "$CONFIG_RS485"
 
-for i in $(echo $CONFIG | jq -r 'keys_unsorted | .[]')
+# Loop through each key in the JSON object
+for i in $(echo "$CONFIG" | sed 's/},{/}\n{/g; s/\[{//; s/}\]$//; s/"//g' | awk -F ':' '{print $1}')
 do
-  if [ $i == "Advanced" ]
+  # Exit the loop when the key is "Advanced"
+  if [ "$i" = "Advanced" ]
   then 
     break
   fi 
-  echo "[$i]" >> $CONFIG_RS485
-  echo $CONFIG | jq --arg id "$i" -r '.[$id]|to_entries|map("\(.key)=\(.value|tostring)")|.[]' | sed -e "s/false/False/g" -e "s/true/True/g" >> $CONFIG_RS485
+  
+  # Write the section header to CONFIG_RS485
+  echo "[$i]" >> "$CONFIG_RS485"
+  
+  # Get the corresponding value for the key
+  value=$(echo "$CONFIG" | sed -n '/'"$i"'/,/},/{/'"$i"'/!{/},/p}}' | awk -F ':' '{print $2}' | sed 's/,//')
+  
+  # Replace "false" with "False" and "true" with "True" in the value
+  value=$(echo "$value" | sed -e "s/false/False/g" -e "s/true/True/g")
+  
+  # Write the key-value pair to CONFIG_RS485
+  echo "$i=$value" >> "$CONFIG_RS485"
 done
-
